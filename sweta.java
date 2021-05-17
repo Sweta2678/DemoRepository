fbabb8ba511b3bca66b38ccc9dacabd9ba0bab32
sweta.khatsuriya <sweta.bhavsar78@gmail.com>
2021-05-17 16:26:07 +0530

    second modified commit

--- sweta.java
+++ sweta.java
@@ -2,3 +2,5 @@ else if (FormatHelper.hasContent(productVOToPersist.getChannelExclusive())
 				&& validateSingleAttribute("CHANNELEXCLUSIVE", productVOToPersist.getChannelExclusive()) == null) {
 			Msg = Msg + " ChannelExclusive " + " With value Key " + productVOToPersist.getChannelExclusive() + " is not Present in Attribute Store";
 			status = false;
+			
+			
